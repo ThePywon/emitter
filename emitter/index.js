cb3e2d852fe8ab95781169add5cc08b71a12d110
Pywon <79721537+ThePywon@users.noreply.github.com>
2022-03-30 18:23:32 +0000

    Big updates yes

--- emitter/index.js
+++ emitter/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const LockedArray = require("./locked_array.js");
-const plus = require("./object-plus.js");
+const LockedArray = require("@protagonists/locked-array");
 
 function Emitter() {
   const events = new LockedArray();
@@ -15,108 +14,107 @@ function Emitter() {
     },
     defaults:{
       enumerable:true,
-      get:()=>{return defaults}
+      get:()=>{return {...defaults}}
     }
   });
 
   Object.defineProperty(this, "setDefault", {
     enumerable:true,
-    value:function Default(name, callback, overridable) {
+    value:(function setDefault(name, callback, overridable) {
       if(typeof name !== "string") throw new Error("Emitter event name must be of type String.");
       if(typeof callback !== "function") throw new Error("Emitter event callback must be of type Function.");
-
+  
       if(defaults[name] && !defaults[name].overridable)
         throw new Error("Default event callback non overridable");
-      
-      defaults[name] = {name,callback,overridable,enabled:!defaults[name]||defaults[name].enabled};
-    },
+        
+      defaults[name] = {callback,overridable:!!overridable,enabled:!defaults[name]||defaults[name].enabled};
+      return this;
+    }).bind(this),
     writable:false
   });
 
   Object.defineProperty(this, "disableDefault", {
     enumerable:true,
-    value:function disableDefault(name) {
-      if(defaults[name])
-        defaults[name].enabled = false;
-    },
+    value:(function disableDefault(name) {
+      if(defaults[name]) defaults[name].enabled = false;
+      return this;
+    }).bind(this),
     writable:false
   });
 
   Object.defineProperty(this, "enableDefault", {
     enumerable:true,
-    value:function enableDefault(name) {
-      defaults[name] ? defaults[name].enabled = true : defaults[name] = {enabled:true};
-    },
+    value:(function enableDefault(name) {
+      if(defaults[name]) defaults[name].enabled = true;
+      else throw new Error("Default event behaviour is undefined!");
+      return this;
+    }).bind(this),
     writable:false
   });
 
+  
+
   Object.defineProperty(this, "on", {
     enumerable:true,
-    value:function on(name, callback) {
+    value:(function on(name, callback) {
       if(typeof name != "string") throw new Error("Emitter event name must be of type String.");
       if(typeof callback != "function") throw new Error("Emitter event callback must be of type Function.");
 
       events.unlock(eventKey);
       events.add({name,callback});
       events.lock();
-    },
+      return this;
+    }).bind(this),
     writable:false
   });
 
   Object.defineProperty(this, "once", {
     enumerable:true,
-    value:function once(name, callback) {
+    value:(function once(name, callback) {
       if(typeof name != "string") throw new Error("Emitter event name must be of type String.");
       if(typeof callback != "function") throw new Error("Emitter event callback must be of type Function.");
 
       events.unlock(eventKey);
       events.add({name,callback,once:true});
       events.lock();
-    },
+    }).bind(this),
     writable:false
   });
 
   Object.defineProperty(this, "emit", {
     enumerable:true,
-    value:function emit(name, ...args) {
+    value:(function emit(name, ...args) {
       if(typeof name != "string") throw new Error("Emitter event name must be of type String.");
     
       if(this.defaults[name] && this.defaults[name].enabled && this.defaults[name].callback)
         this.defaults[name].callback(...args);
 
-      let deletion = [];
       for(let i = 0; i < this.events.length; i++)
         if(this.events[i].name === name)
           if(this.events[i].once) {
-            deletion.push(this.events[i]);
-            this.events[i].callback(...args);
+            const callback = this.events[i].callback;
+            events.unlock(eventKey);
+            events.remove(this.events[i]);
+            events.lock();
+            i--;
+            callback(...args);
           }
           else this.events[i].callback(...args);
-      
-      for(let i = 0; i < deletion.length; i++) {
-        events.unlock(eventKey);
-        events.remove(this.events[i]);
-        events.lock();
-      }
-    },
+
+      return this;
+    }).bind(this),
     writable:false
   });
 }
 
-Object.defineProperty(Emitter, "setEmitter", {
-  enumerable:true,
-  value:function setEmitter(obj) {
-    plus.inheritBaseProperties(obj, new Emitter());
-  },
-  writable:false
-});
+Emitter.setEmitter = Emitter.call;
 
 Object.defineProperties(Emitter.prototype, {
   valueOf:{
-    value:function valueOf() {return this.events}
+    value:(function valueOf() {return this.events}).bind(Emitter.prototype)
   },
   toString:{
-    value:function toString() {return "[object Emitter]"}
+    value:(function toString() {return "[object Emitter]"}).bind(Emitter.prototype)
   }
 });
 
